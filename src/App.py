329cf279888ad6bb6b42feaa698ac32c00d02cb9
Jim Nguyen <jim.nguyen2017@gmail.com>
2022-08-07 14:05:47 -0700

    Added Insertion Sort Algorithm Implementation

--- src/App.py
+++ src/App.py
@@ -1,10 +1,15 @@
 from Algorithms.BinarySearch import *
 from MapCreatorPygame.BinarySearchMapGenerator import *
+from Algorithms.InsertionSort import *
+from MapCreatorPygame.InsertionSortMapGenerator import *
 
 
 def main():
     map = BinarySearchMapGenerator()
     print(binarySearch(map, 1))
+
+    insertionMap = InsertionSortMapGenerator()
+    insertionSort(insertionMap)
 
 if __name__ == "__main__":
     main()
---
+++ src/MapCreatorPygame/InsertionSortMapGenerator.py
@@ -0,0 +1,35 @@
+class InsertionSortMapGenerator(object):
+    """
+    This class is generate and update Array that we are sorting.
+    Flow of the class
+    first enter # of integers that will be in the array until type End to end the input
+    the array will then start to sort itself
+    """
+    def __init__(self) -> None:
+        integerArrayInput = input("Please enter integers you want sorted. Type in End to stop inputting integers: ")
+        inputArray = []
+        while integerArrayInput != "End":
+            inputArray.append(int(integerArrayInput)) 
+            integerArrayInput = input()
+        self.array = inputArray
+        self.inputArraySize = len(inputArray)
+
+    def getArray(self):
+        return self.array
+
+    def getArraySize(self):
+        return self.inputArraySize
+
+    def swapValuesAtIndices(self, indexOne, indexTwo):
+        self.array[indexOne], self.array[indexTwo] = self.array[indexTwo], self.array[indexOne]
+    
+    def valueAtIndex(self, index):
+        return self.array[index]
+
+    
+
+"""
+Resources used to help me
+https://www.w3schools.com/python/ref_func_input.asp
+https://stackoverflow.com/questions/55671872/python-comparison-if-statement-not-working
+"""

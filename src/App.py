4270e07b1d94ef21956448f24d91b6e8490348d1
Yoan Palacios G <44234291+PalaciosYoan@users.noreply.github.com>
2022-08-07 21:06:06 -0700

    Added AppLayOut Folder

--- src/App.py
+++ src/App.py
@@ -1,10 +1,14 @@
-from Algorithms.BinarySearch import *
-from MapCreatorPygame.BinarySearchMapGenerator import *
+# from Algorithms.BinarySearch import *
+# from MapCreatorPygame.BinarySearchMapGenerator import *
 
 
-def main():
-    map = BinarySearchMapGenerator()
-    print(binarySearch(map, 1))
+# def main():
+#     map = BinarySearchMapGenerator()
+#     print(binarySearch(map, 1))
+
+from AppLayOut.MainMenuLayOut import *
 
 if __name__ == "__main__":
-    main()
+    helloKivy = HelloKivy()
+    helloKivy.run()
+    #main()
---
+++ src/AppLayOut/MainMenuLayOut.py
@@ -0,0 +1,8 @@
+import kivy
+
+from kivy.app import App 
+from kivy.uix.button import Label
+
+class HelloKivy(App):
+    def build(self):
+        return Label(text="Hello World")

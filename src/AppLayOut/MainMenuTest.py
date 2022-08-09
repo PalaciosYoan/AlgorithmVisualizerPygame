f83c2d73b55b41aa91ee21780e6693d96e6cd452
Jim Nguyen <jim.nguyen2017@gmail.com>
2022-08-08 19:27:42 -0700

    Got the animation to work
    However, the color still needs to be fixed

--- src/AppLayOut/MainMenuTest.py
+++ src/AppLayOut/MainMenuTest.py
@@ -1,13 +1,18 @@
+import threading
+import time
+from functools import partial
+
 from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.screenmanager import Screen, ScreenManager, FadeTransition
 from kivy.uix.label import Label
+from kivy.properties import NumericProperty
+from kivy.clock import Clock, mainthread
 from Algorithms.InsertionSort import *
 from MapCreatorPygame.InsertionSortMapGenerator import *
-from kivy.properties import NumericProperty
-from kivy.clock import Clock
+
 
 
 class ScreenManagement(ScreenManager):
@@ -34,7 +39,7 @@ class SelectionScreen(Screen):
         self.manager.current = 'Insertion Sort'
 
 class InsertionSortScreen(Screen):
-    index = NumericProperty(1)
+    stop = threading.Event()
     def __init__(self, **kwargs):
         super(InsertionSortScreen, self).__init__(**kwargs)
         self.insertionMap = InsertionSortMapGenerator()
@@ -47,19 +52,20 @@ class InsertionSortScreen(Screen):
         insertionSortDescription = Label(text = "Insertion Sort Method", size_hint = (0.5, 0.25))
         self.styleLayout.add_widget(insertionSortDescription)
         startButton = Button(text = 'Hello World', size_hint = (0.5, 0.25))
-        startButton.bind(on_press = self.pressed)
+        startButton.bind(on_press = self.start_second_thread)
         self.styleLayout.add_widget(startButton)
         self.add_widget(self.styleLayout)
 
+    def start_second_thread(self, *args):
+        threading.Thread(target=self.pressed).start()
 
     def pressed(self, *args):
-        if self.index < self.insertionMap.getArraySize():
-            traversalIndex = self.index
+        for index in range(1,self.insertionMap.getArraySize()):
+            traversalIndex = index
             currentIndexForLayout = -1 * (traversalIndex + 1)
             previousIndexForLayout = -1 * traversalIndex
-            self.insertionLayout.children[previousIndexForLayout].color = (0,1,0,1)
-            self.insertionLayout.children[currentIndexForLayout].color = (0,1,0,1)
-            self.styleLayout.children[1].text = "Comparing these two values: " + self.insertionLayout.children[previousIndexForLayout].text + " and " + self.insertionLayout.children[currentIndexForLayout].text
+            Clock.schedule_once(partial(self.changeTextAndColor, currentIndexForLayout, previousIndexForLayout),0)
+            time.sleep(1)
             print("Comparing these two values Layout", self.insertionLayout.children[previousIndexForLayout].text, self.insertionLayout.children[currentIndexForLayout].text)
             #print("Comparing these two values Print", self.insertionMap.valueAtIndex(traversalIndex - 1), self.insertionMap.valueAtIndex(traversalIndex))
 
@@ -67,19 +73,37 @@ class InsertionSortScreen(Screen):
             while traversalIndex > 0 and self.insertionMap.valueAtIndex(traversalIndex - 1) > self.insertionMap.valueAtIndex(traversalIndex):
                 print("Swapping these two values Print:", self.insertionMap.valueAtIndex(traversalIndex - 1), self.insertionMap.valueAtIndex(traversalIndex))
                 #print("Swapping these two values Layout", self.insertionLayout.children[previousIndexForLayout].text, self.insertionLayout.children[currentIndexForLayout].text)
-                self.styleLayout.children[1].text = "Swapping these two values: " + self.insertionLayout.children[previousIndexForLayout].text + " and " + self.insertionLayout.children[currentIndexForLayout].text
-                self.insertionMap.swapValuesAtIndices(traversalIndex - 1, traversalIndex)
-                self.insertionLayout.children[previousIndexForLayout].text, self.insertionLayout.children[currentIndexForLayout].text = self.insertionLayout.children[currentIndexForLayout].text, self.insertionLayout.children[previousIndexForLayout].text
-                self.insertionLayout.children[previousIndexForLayout].color = (1,1,1,1)
-                self.insertionLayout.children[currentIndexForLayout].color = (1,1,1,1)
+                Clock.schedule_once(partial(self.swapTextAndChangeTextAndColor, traversalIndex, currentIndexForLayout, previousIndexForLayout),0)
+                time.sleep(1)
                 traversalIndex -= 1
                 currentIndexForLayout += 1
                 previousIndexForLayout += 1
-            
-            self.index += 1
-            Clock.schedule_once(self.pressed, 2)
+            Clock.schedule_once(partial(self.changeColor, currentIndexForLayout, previousIndexForLayout),0)
+            time.sleep(1)
 
+    @mainthread
+    def changeTextAndColor(self, currentIndexForLayout, previousIndexForLayout, *args):
+        self.styleLayout.children[1].text = "Comparing these two values: " + self.insertionLayout.children[previousIndexForLayout].text + " and " + self.insertionLayout.children[currentIndexForLayout].text
+        self.insertionLayout.children[previousIndexForLayout].color = (0,1,0,1)
+        self.insertionLayout.children[currentIndexForLayout].color = (0,1,0,1)
+    
+    @mainthread
+    def swapTextAndChangeTextAndColor(self, traversalIndex, currentIndexForLayout, previousIndexForLayout, *args):
+        self.insertionLayout.children[previousIndexForLayout].color = (0,1,0,1)
+        self.insertionLayout.children[currentIndexForLayout].color = (0,1,0,1)
+        self.styleLayout.children[1].text = "Swapping these two values: " + self.insertionLayout.children[previousIndexForLayout].text + " and " + self.insertionLayout.children[currentIndexForLayout].text
+        self.insertionMap.swapValuesAtIndices(traversalIndex - 1, traversalIndex)
+        self.insertionLayout.children[previousIndexForLayout].text, self.insertionLayout.children[currentIndexForLayout].text = self.insertionLayout.children[currentIndexForLayout].text, self.insertionLayout.children[previousIndexForLayout].text
+
+    @mainthread
+    def changeColor(self, currentIndexForLayout, previousIndexForLayout, *args):
+        self.insertionLayout.children[previousIndexForLayout].color = (1,1,1,1)
+        self.insertionLayout.children[currentIndexForLayout].color = (1,1,1,1)
+        
 class MyApp(App):
+    def on_stop(self):
+        self.root.stop.set()
+
     def build(self):
         windowManager = ScreenManagement(transition = FadeTransition())
         windowManager.add_widget(SelectionScreen(name = 'Selection Screen'))
@@ -89,6 +113,7 @@ class MyApp(App):
 #https://stackoverflow.com/questions/57487117/some-troubles-using-screen-manager-without-kv-file
 #https://stackoverflow.com/questions/31639452/kivy-access-child-id
 #https://stackoverflow.com/questions/63867627/how-do-i-update-the-text-in-label-widget-within-the-for-loop
+#https://github.com/kivy/kivy/wiki/Working-with-Python-threads-inside-a-Kivy-application
 
 
 """
